53545dc4103f6b4f7881dc63dae3f5f9ca6cb8cc
Aarni Koskela <akx@iki.fi>
2017-10-05 15:37:09 +0300

    Add --currency-multiplier

--- billtag.py
+++ billtag.py
@@ -25,7 +25,7 @@ def fraction_as_decimal(value):
     return Decimal(value.numerator) / Decimal(value.denominator)
 
 
-def process(data, rounding=2):
+def process(data, rounding=2, currency_multiplier=1):
     by_tag = defaultdict(list)
     total_price = 0
     for line in data:
@@ -36,6 +36,7 @@ def process(data, rounding=2):
             line_price = parse_decimal(line['unit']) * qty
         else:
             raise NotImplementedError('no total or unit in line %r' % line)
+        line_price *= currency_multiplier
         total_price += line_price
         name = line['name']
         tags = Counter(line['tags'])
@@ -79,10 +80,11 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('input')
     ap.add_argument('-r', '--rounding', default=2, type=int)
+    ap.add_argument('-c', '--currency-multiplier', default=1, type=parse_decimal)
     args = ap.parse_args()
     with open(args.input) as infp:
         data = list(read_tsv(infp))
-    processed = process(data, rounding=args.rounding)
+    processed = process(data, rounding=args.rounding, currency_multiplier=args.currency_multiplier)
     print_itemization(processed)
     print('[>] Total price: %s' % processed['total_split_price'])
     rounding_remainder = processed['total_price'] - processed['total_split_price']
