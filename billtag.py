de07ff4a33a68d6dd0de6a6862fafd2282c2e546
Aarni Koskela <akx@iki.fi>
2017-10-05 15:51:29 +0300

    Add --delivery

--- billtag.py
+++ billtag.py
@@ -25,9 +25,10 @@ def fraction_as_decimal(value):
     return Decimal(value.numerator) / Decimal(value.denominator)
 
 
-def process(data, rounding=2, currency_multiplier=1):
+def process(data, rounding=2, currency_multiplier=1, delivery=0):
     by_tag = defaultdict(list)
     total_price = 0
+    tag_shares = Counter()
     for line in data:
         try:
             qty = int(line['qty'])
@@ -51,6 +52,7 @@ def process(data, rounding=2, currency_multiplier=1):
         total_tags = sum(tags.values())
         for tag, count in tags.items():
             share = Fraction(count, total_tags)
+            tag_shares[tag] += share
             by_tag[tag].append({
                 'line': line,
                 'name': name,
@@ -61,7 +63,24 @@ def process(data, rounding=2, currency_multiplier=1):
                 'split_price': round(Decimal(float(share)) * line_price, rounding),
             })
 
+    if delivery != 0:
+        share_total = sum(tag_shares.values())
+        delivery *= currency_multiplier
+        total_price += delivery
+        for tag, share in tag_shares.items():
+            share /= share_total
+            by_tag[tag].append({
+                'line': None,
+                'name': '* Delivery',
+                'share': share,
+                'total_qty': 1,
+                'split_qty': share,
+                'total_price': delivery,
+                'split_price': round(delivery * Decimal(float(share)), rounding),
+            })
+
     return {
+        'tag_shares': tag_shares,
         'by_tag': by_tag,
         'total_price': total_price,
         'total_split_price': sum(i['split_price'] for i in chain(*by_tag.values())),
@@ -77,7 +96,7 @@ def print_itemization(processed):
         print('=' * len(header_line))
         name_len = max(len(item['name']) for item in tag_items)
         for item in sorted(tag_items, key=itemgetter('name')):
-            print('%-*s | %4s | %6s' % (
+            print('%-*s | %6s | %6s' % (
                 name_len, item['name'],
                 item['split_qty'],
                 item['split_price'],
@@ -89,10 +108,11 @@ def main():
     ap.add_argument('input')
     ap.add_argument('-r', '--rounding', default=2, type=int)
     ap.add_argument('-c', '--currency-multiplier', default=1, type=parse_decimal)
+    ap.add_argument('-d', '--delivery', default=0, type=parse_decimal, help='add total delivery (in foreign currency), weighted by line total')
     args = ap.parse_args()
     with open(args.input) as infp:
         data = list(read_tsv(infp))
-    processed = process(data, rounding=args.rounding, currency_multiplier=args.currency_multiplier)
+    processed = process(data, rounding=args.rounding, currency_multiplier=args.currency_multiplier, delivery=args.delivery)
     print_itemization(processed)
     print('[>] Total price: %s' % processed['total_split_price'])
     rounding_remainder = processed['total_price'] - processed['total_split_price']
